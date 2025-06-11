9101438ba9feafb24c7a5e6baab90dc8bc720300
AKARuberDuck <ss2875356@gmail.com>
2025-06-11 11:45:57 -0500

    Update games.js

--- api/games.js
+++ api/games.js
@@ -1,1 +1,6 @@
-
+export default async function handler(req, res) {
+  const response = await fetch("https://www.freetogame.com/api/games");
+  const data = await response.json();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).json(data);
+}
